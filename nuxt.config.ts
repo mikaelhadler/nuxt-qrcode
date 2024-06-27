b03e02e8a3b817445a12eabca2e7255cedb85cff
Mikael Hadler <mikaelhadler@gmail.com>
2024-06-27 12:23:28 -0300

    (feature): analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/public/main.css'],
+  css: ["~/public/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+  modules: ["@nuxtjs/robots", ["nuxt-gtag", { id: "G-YR3VS1VKXQ" }]],
+});
