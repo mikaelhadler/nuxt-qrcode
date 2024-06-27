a0dc4a67e6c390c38087ab593dcd1f9fb6f83961
Mikael Hadler <mikaelhadler@gmail.com>
2024-06-27 11:48:47 -0300

    (refactor): improve ux

--- app.vue
+++ app.vue
@@ -3,9 +3,29 @@ import VueQrcode from "@chenfengyuan/vue-qrcode";
 import { toPng } from "html-to-image";
 import "~/public/main.css";
 
+useHead({
+  title: "QR Code Generator",
+  meta: [
+    {
+      name: "description",
+      content: "Generate QR code for everything you want to share"
+    },
+    {
+      name: "keywords",
+      content: "QR code, QR code generator, QR code for "
+    }
+  ]
+});
+useSeoMeta({
+  title: "QR Code Generator",
+  ogTitle: "QR Code Generator",
+  ogDescription: "Generate QR code for everything",
+  description: "Generate QR code for everything",
+  keywords: "QR code, QR code generator, QR code for everything"
+});
+
 const link = ref("");
 const qrCodeValue = ref("");
-const isInvalid = ref(false);
 const downloading = ref(false);
 const templateReference = ref(null);
 
@@ -35,40 +55,17 @@ const keyPressHandler = (e) => {
 const generateQrCode = () => {
   qrCodeValue.value = link.value;
 };
-const isValidUrl = urlString => {
-  var urlPattern = new RegExp('^(https?:\\/\\/)?' +
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-    '((\\d{1,3}\\.){3}\\d{1,3}))' +
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-    '(\\?[;&a-z\\d%_.~+=-]*)?' +
-    '(\\#[-a-z\\d_]*)?$', 'i');
-  return !!urlPattern.test(urlString);
-}
-watch(link, (value) => {
-  if (value && !isValidUrl(value)) {
-    isInvalid.value = true;
-  } else {
-    isInvalid.value = false;
-  }
-});
-
 </script>
 <template>
   <div class="flex w-fit mx-auto min-h-full flex-col justify-center items-center px-6 py-12 lg:px-8">
     <h1 class="text-2xl font-bold mt-4">QR Code Generator</h1>
-    <p class="text-gray-500 mt-2">Enter a link to generate QR code</p>
-    <input type="text" v-model="link"
-      :class="isInvalid ? 'border-red-500 text-red-500 border outline-red-500' : 'border-gray-300'"
-      class="w-80 mt-4 p-2 border border-gray-300 rounded-md" placeholder="Enter a link to generate QR code"
-      spellcheck="false" @keyup.enter="keyPressHandler" />
-    <div class="flex w-full mt-1">
-      <p class="text-red-500 text-xs" v-if="isInvalid">Invalid URL</p>
-    </div>
+    <p class="text-gray-500 mt-2">Enter what you want to generate QR code</p>
+    <input type="text" v-model="link" class="w-80 mt-4 p-2 border border-gray-300 rounded-md" spellcheck="false"
+      @keyup.enter="keyPressHandler" />
     <div class="flex justify-center mt-4">
       <button target="_blank" rel="noopener"
         class="w-80 bg-yellow-400 rounded-lg text-gray-800 font-medium text-base md:text-lg py-3 px-8 md:px-12 hover:bg-yellow-500 transition-all duration-150 ease-in-out"
-        :class="!link || isInvalid ? 'cursor-not-allowed' : 'cursor-pointer'" :disabled="!link || isInvalid"
-        @click="generateQrCode">
+        :class="!link ? 'cursor-not-allowed' : 'cursor-pointer'" :disabled="!link" @click="generateQrCode">
         Generate QR Code
       </button>
     </div>
@@ -77,7 +74,7 @@ watch(link, (value) => {
       <div v-if="qrCodeValue" ref="templateReference">
         <VueQrcode :value="qrCodeValue" :options="{ width: 250, height: 250 }" />
       </div>
-      <p class="mt-4 font-bold">Scan the QR code to visit the link</p>
+      <p class="mt-4 font-bold">Scan the QR code</p>
       <button target="_blank" rel="noopener"
         class="mt-4 w-64 bg-yellow-400 rounded-lg text-gray-800 font-medium text-base md:text-lg py-3 px-8 md:px-12 hover:bg-yellow-500 transition-all duration-150 ease-in-out"
         @click="downloadQRCode">
